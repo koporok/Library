7a3a276befce44022366365f02c6153132f28a6f
koporok <122740702+koporok@users.noreply.github.com>
2023-11-01 13:32:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notes/DataBase.java
@@ -0,0 +1,56 @@
+package com.example.notes;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class DataBase {
+    static DataBase instance;
+    static SharedPreferences sharedPref;
+    private DataBase(Activity t){
+        sharedPref = t.getPreferences(Context.MODE_PRIVATE);
+    }
+
+    static DataBase createInstance(Activity t){
+        if(instance == null){
+            instance = new DataBase(t);
+        }
+        return instance;
+    }
+
+    static Boolean info(){
+        if(sharedPref.contains("listOfItem")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void addItem(ArrayList<Zametka> list){
+        Gson gson = new Gson();
+        String jsonList = gson.toJson(list);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("listOfItem", jsonList);
+        editor.apply();
+    }
+
+    ArrayList<Zametka> getItem(String name){
+        String jsonList = sharedPref.getString("listOfItem", null);
+        if (jsonList != null) {
+            Gson gson = new Gson();
+            Type type = new TypeToken<ArrayList<Zametka>>() {}.getType();
+            ArrayList<Zametka> list = gson.fromJson(jsonList, type);
+            return list;
+        }
+        return null;
+    }
+
+    static DataBase getInstance(){
+        return instance;
+    }
+}
---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.notes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int NOTE_ACTIVITY_REQUEST_CODE = 1;
+    public ArrayList<Zametka> NoteList;
+    Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DataBase.createInstance(this);
+        if(DataBase.info()){
+            NoteList = DataBase.getInstance().getItem("listOfItem");
+        } else {
+            NoteList = new ArrayList<>();
+        }
+
+        ListView listView = findViewById(R.id.listItem);
+        adapter = new Adapter(this, android.R.layout.simple_expandable_list_item_1, NoteList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, v, position, id) -> {
+            Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+            intent.putExtra("item", NoteList.get(position));
+            intent.putExtra("position", position);
+            startActivityForResult(intent, NOTE_ACTIVITY_REQUEST_CODE);
+        });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            NoteList.remove(position);
+            adapter.notifyDataSetChanged();
+            return false;
+        });
+    }
+
+    public void onClickButtonAdd(View view){
+        Zametka a = new Zametka("Заметка", "Текст заметки");
+        NoteList.add(a);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NOTE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK && data != null) {
+                Zametka newItem = (Zametka) data.getSerializableExtra("newItem");
+                int position1 = data.getIntExtra("position1", -1);
+                NoteList.set(position1, newItem);
+                DataBase.getInstance().addItem(NoteList);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+
+}
